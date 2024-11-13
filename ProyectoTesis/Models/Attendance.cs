64b381a4003871b92ea88f164dca428c7ab7117b
aaron acuña <aaronacu_85@hotmail.com>
2024-11-13 16:08:33 -0500

    feat(project): added models.

---
+++ ProyectoTesis/Models/Attendance.cs
@@ -0,0 +1,32 @@
+﻿namespace ProyectoTesis.Models
+{
+    public class Attendance
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+        public int MinuteLate { get; set; }
+
+        public Attendance()
+        {
+            this.Id = 0;
+            this.FirstName = string.Empty;
+            this.LastName = string.Empty;
+            this.MinuteLate = 0;
+        }
+        public Attendance
+            (int id, string firstName, string lastName,
+            DateTime checkIn, DateTime checkOut,
+            int minuteLate)
+        {
+            this.Id = id;
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.CheckIn = checkIn;
+            this.CheckOut = checkOut;
+            this.MinuteLate = minuteLate;
+        }
+    }
+}
---
+++ ProyectoTesis/Models/Employee.cs
@@ -0,0 +1,30 @@
+﻿namespace ProyectoTesis.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string JobDepartment { get; set; }
+        public string JobPosition { get; set; }
+
+        public Employee()
+        {
+            this.Id = 0;
+            this.FirstName = string.Empty;
+            this.LastName = string.Empty;
+            this.JobDepartment = string.Empty;
+            this.JobPosition = string.Empty;
+        }
+        public Employee
+            (int id, string firstName, string lastName,
+            string jobDepartment, string jobPosition)
+        {
+            this.Id = id;
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.JobDepartment = jobDepartment;
+            this.JobPosition = jobPosition;
+        }
+    }
+}
