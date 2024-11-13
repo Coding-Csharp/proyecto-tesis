585dc98a82311d66de65e3ea25ea2787156302f1
aaron acuña <aaronacu_85@hotmail.com>
2024-11-13 11:56:49 -0500

    feat(access): added cache configuration.

---
+++ ProyectoTesis/Controllers/AccessController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProyectoTesis.Controllers
+{
+    [AllowAnonymous]
+    public class AccessController : Controller
+    {
+        [HttpGet]
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Login()
+        {
+            if (User?.Identity?.IsAuthenticated is true)
+            {
+                if (User.IsInRole("ADMIN"))
+                    return RedirectToAction("InterfaceAdmin", "Admins");
+
+                else if (User.IsInRole("EMPLOYEE"))
+                    return RedirectToAction("InterfaceEmployee", "Employees");
+            }
+
+            return View();
+        }
+    }
+}
