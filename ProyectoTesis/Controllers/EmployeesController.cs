a38ba001f3ed11d6cd027f153cd7b8445a37752f
aaron acuña <aaronacu_85@hotmail.com>
2024-11-13 16:10:50 -0500

    feat(project): added employee controller.

---
+++ ProyectoTesis/Controllers/EmployeesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProyectoTesis.Controllers
+{
+    //[Authorize(Roles = "TRABAJADOR")]
+    public class EmployeesController : Controller
+    {
+        [HttpGet]
+        public IActionResult InterfaceEmployee() => View();
+
+        [HttpGet]
+        public IActionResult AttendanceList() => View();
+    }
+}
