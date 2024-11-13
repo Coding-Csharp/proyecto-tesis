fed3054d18f54e79642dc4e1be06c1b528e37aeb
aaron acuña <aaronacu_85@hotmail.com>
2024-11-13 11:55:58 -0500

    feat(home): added level security.

--- ProyectoTesis/Controllers/HomeController.cs
+++ ProyectoTesis/Controllers/HomeController.cs
@@ -1,7 +1,9 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ProyectoTesis.Controllers
 {
+    [AllowAnonymous]
     public class HomeController : Controller
     {
         // VIEW TO CREATE A BUSINESS LANDING PAGE (IT'S EMPTY)
