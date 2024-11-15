dd0485d977127beadf0da0e66d3d02493f10f1fd
aaron acuña <aaronacu_85@hotmail.com>
2024-11-14 20:27:02 -0500

    feat(access): added logic for authentication.

--- ProyectoTesis/Controllers/AccessController.cs
+++ ProyectoTesis/Controllers/AccessController.cs
@@ -1,11 +1,18 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using System.Security.Claims;
+using ProyectoTesis.Models;
 
 namespace ProyectoTesis.Controllers
 {
     [AllowAnonymous]
     public class AccessController
-        (IWebHostEnvironment webHostEnvironment) :
+        (TesisContext context,
+        IWebHostEnvironment webHostEnvironment) :
         Controller
     {
         private readonly IWebHostEnvironment _webHostEnvironment = webHostEnvironment;
@@ -24,6 +31,50 @@ namespace ProyectoTesis.Controllers
             }
 
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login
+            (dynamic credential)
+        {
+            string role = credential.Role;
+            string id = credential.Id;
+            string code = credential.Code;
+
+            dynamic? result = role switch
+            {
+                "ADMINISTRADOR" =>
+                await context.Set<AdminCredential>()
+                .Where(a => a.AdminsId == id && a.Code == code)
+                .FirstOrDefaultAsync(),
+
+                "TRABAJADOR" =>
+                await context.Set<EmployeeCredential>()
+                .Where(a => a.EmployeesId == id && a.Code == code)
+                .FirstOrDefaultAsync(),
+
+                _ => null
+            };
+
+            if (result is null)
+                return Content(JsonConvert.SerializeObject
+                    (false), "application/json");
+
+            List<Claim> claims =
+            [
+                new(ClaimTypes.Role, role),
+                new(ClaimTypes.Name, id)
+            ];
+
+            ClaimsIdentity claimsIdentity = new(claims,
+                CookieAuthenticationDefaults.AuthenticationScheme);
+
+            await HttpContext.SignInAsync
+                (CookieAuthenticationDefaults.AuthenticationScheme,
+                new ClaimsPrincipal(claimsIdentity));
+
+            return Content(JsonConvert.SerializeObject
+                (true), "application/json");
         }
     }
 }
